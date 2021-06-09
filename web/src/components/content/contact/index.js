9953ce1a53636765a4da681596bbc0413387522f
Kein-chronicle <keinclonicle@gmail.com>
2021-06-09 10:13:20 +0900

    change contact info

--- web/src/components/content/contact/index.js
+++ web/src/components/content/contact/index.js
@@ -4,10 +4,11 @@ function Contact() {
     return (
         <div style={{paddingTop:'20px'}}>
             <div style={{fontWeight:'bolder', paddingBottom:'7px'}}>Phone</div>
-            <div style={{paddingBottom:'30px'}}>+82-10-4040-1824</div>
+            <div style={{paddingBottom:'30px'}}>+82-10-4044-1824</div>
             <div style={{fontWeight:'bolder', paddingBottom:'7px'}}>Email</div>
             <div style={{paddingBottom:'30px'}}>k_star_w@naver.com</div>
             <div><a href="https://www.linkedin.com/in/kein-chronicle-kim-39345b13a/">Linked in</a></div>
+            <div><a href="https://www.youtube.com/channel/UCv8cGlH7g0UBgnHQVsY9zDg">YouTube</a></div>
             <div style={{color:'gray'}}>
                 Do you want to build any service?<br />
                 Mail to Me
